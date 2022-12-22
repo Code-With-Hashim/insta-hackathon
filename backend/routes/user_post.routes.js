6bad38c9e93b12d50152fcda490da1ea00f06d5b
Arun Singh <95880659+arun-developers@users.noreply.github.com>
2022-12-22 16:13:31 +0530

    Get Users

--- backend/routes/user_post.routes.js
+++ backend/routes/user_post.routes.js
@@ -1,9 +1,24 @@
 const express= require('express');
 const {user_post_modal} = require("../modals/user_post.modal");
 const PostRouter = express.Router();
+
+PostRouter.get("/", async (req, res) => {
+    const params = req.query
+    try {
+        const users = await user_post_modal.find(params)
+        res.status(200).json(users)
+    }
+    catch (err) {
+        console.log(err)
+        res.status(401).send({
+            message: "Users Data not found",
+            status: "Failed"
+        })
+    }
+
+})
+
 // creating a PostRouter
-
-
 PostRouter.post("/create",async(req,res)=>{
     const payload = req.body;
     console.log(payload);
