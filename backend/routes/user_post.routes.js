b7c08506dc12864affecb58a23abb6917af64f4f
Param1Raj <pr6587424@gmail.com>
2022-12-22 16:15:07 +0530

    post api complete

--- backend/routes/user_post.routes.js
+++ backend/routes/user_post.routes.js
@@ -2,14 +2,37 @@ const express= require('express');
 const {user_post_modal} = require("../modals/user_post.modal");
 const PostRouter = express.Router();
 // creating a PostRouter
+var multer  = require('multer')
+require('dotenv').config();
+const cloudinary = require('cloudinary').v2;
 
+// Return "https" URLs by setting secure: true
+cloudinary.config({
+  cloud_name :process.env.cloud_name,
+  api_key:process.env.api_key,
+  api_secret:process.env.api_secret
+});
 
-PostRouter.post("/create",async(req,res)=>{
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+var upload = multer({ storage: storage })
+
+PostRouter.post("/create",upload.single('post_image'),async(req,res)=>{
     const payload = req.body;
     console.log(payload);
     if(payload.username &&  payload.user_id){
+        let result = await cloudinary.uploader.upload(req.file.path);
+        // console.log(result);
+        payload.img = result.url;
         let data = await user_post_modal(payload);
         data.save();
+        // res.send(payload);
         res.send({msg:"Post created successfully!"})
     }else{
         res.status(400).send({msg:"Something wents wrong"});
